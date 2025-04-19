6f9f803b74c8a8548e06e7c369905d83aed284ed
Drauwood <drauwood@gmail.com>
2025-04-19 05:21:49 +0300

    Add nvim lsp

--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -26,6 +26,7 @@ require("lazy").setup({
         }       
     },
     { 'nvim-treesitter/nvim-treesitter' },
+    { 'neovim/nvim-lspconfig' },
     { 'navarasu/onedark.nvim' },
     { 
         'nvim-telescope/telescope.nvim',
